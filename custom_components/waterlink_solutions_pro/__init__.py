70528bb2b9b0729bb5ce206d75b44acfb87ba216
Al Weddell <al@fluxpowered.com>
2025-08-01 19:47:48 +1000

    Fix async config setup issue

--- custom_components/waterlink_solutions_pro/__init__.py
+++ custom_components/waterlink_solutions_pro/__init__.py
@@ -1,8 +1,12 @@
 from .const import DOMAIN
 from .api import WaterLinkAPI
 from .coordinator import WaterLinkDataCoordinator
+import logging
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry):
+    _LOGGER.debug("Setting up WaterLink Solutions Pro integration")
     api = WaterLinkAPI(
         entry.data["username"],
         entry.data["password"],
@@ -10,13 +14,15 @@ async def async_setup_entry(hass, entry):
     )
     coordinator = WaterLinkDataCoordinator(hass, api)
     await coordinator.async_config_entry_first_refresh()
+    _LOGGER.debug("Coordinator initial data: %s", coordinator.data)
 
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
     await hass.config_entries.async_unload_platforms(entry, ["sensor"])
     hass.data[DOMAIN].pop(entry.entry_id)
     return True
+
